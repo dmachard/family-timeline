65b0d93ca0c6d71a1ff4a1a77244d9aa409bfa76
dmachard <5562930+dmachard@users.noreply.github.com>
2024-08-24 11:49:51 +0200

    add timeout config ffor axios instance

--- vuejs-client/public/config.js
+++ vuejs-client/public/config.js
@@ -1,5 +1,6 @@
 window.config = {
   language: 'fr',
   startYear: 1700,
-  endYear: 2030
+  endYear: 2030,
+  axiosTimeout: 5000
 }
--- vuejs-client/src/services/axiosInstance.js
+++ vuejs-client/src/services/axiosInstance.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import config from '../config'
 
 const apiClient = axios.create({
   baseURL: import.meta.env.MODE === 'development'
     ? import.meta.env.VITE_API_URL
     : '/api',
-  timeout: 1000,
+  timeout: config.axiosTimeout || 5000,
   headers: { 'Content-Type': 'application/json' }
 })
 
