bf837e005dd205b156d2f90ac1ec9da24b16402b
dmachard <5562930+dmachard@users.noreply.github.com>
2024-09-01 16:14:31 +0200

    full screen on mobile for modal fix some bugs on mobile only

--- vuejs-client/src/components/ModalActivities.vue
+++ vuejs-client/src/components/ModalActivities.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="activitiesModal" class="modal fade" tabindex="-1" aria-labelledby="activitiesModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg modal-dialog-scrollable modal-fullscreen-sm-down">
       <div class="modal-content">
         <div class="modal-header">
           <h5 id="activitiesModalLabel" class="modal-title">
@@ -19,7 +19,9 @@
             <table class="table table-hover bg-white mb-4">
               <thead>
                 <tr>
-                  <th scope="col">#</th>
+                  <th scope="col">
+                    #
+                  </th>
                   <th>{{ $t('user') }}</th>
                   <th>{{ $t('timestamp') }}</th>
                   <th>{{ $t('details') }}</th>
@@ -37,27 +39,23 @@
           </div>
 
           <!-- Pagination and Controls -->
-          <div class="d-flex justify-content-between align-items-center mb-3">
-            <!-- Items Per Page -->
-            <div class="d-flex align-items-center me-3">
-              <label for="itemsPerPage" class="me-2 mb-0">{{ $t('items-per-page') }}</label>
-              <select id="itemsPerPage" v-model="itemsPerPage" class="form-select d-inline-block w-auto">
-                <option v-for="size in [5, 10, 15, 20]" :key="size" :value="size">
-                  {{ size }}
-                </option>
-              </select>
-            </div>
-
+          <div class="d-flex flex-column flex-sm-row justify-content-between align-items-center mb-3">
             <!-- Pagination -->
             <nav aria-label="Page navigation">
-              <ul class="pagination mb-0">
+              <ul class="pagination mb-3 mb-sm-0">
                 <li class="page-item" :class="{ disabled: currentPage === 1 }">
                   <a class="page-link" href="#" aria-label="Previous" @click.prevent="previousPage">
                     <span aria-hidden="true">&laquo;</span>
                   </a>
                 </li>
-                <li v-for="page in totalPages" :key="page" class="page-item" :class="{ active: currentPage === page }">
-                  <a class="page-link" href="#" @click.prevent="changePage(page)">{{ page }}</a>
+                <li
+                  v-for="(page, index) in visiblePages" :key="index" class="page-item" 
+                  :class="{ active: currentPage === page, disabled: page === '...' }"
+                >
+                  <a v-if="page !== '...'" class="page-link" href="#" @click.prevent="changePage(page)">
+                    {{ page }}
+                  </a>
+                  <span v-else class="page-link">...</span>
                 </li>
                 <li class="page-item" :class="{ disabled: currentPage === totalPages }">
                   <a class="page-link" href="#" aria-label="Next" @click.prevent="nextPage">
@@ -66,6 +64,16 @@
                 </li>
               </ul>
             </nav>
+
+            <!-- Items Per Page -->
+            <div class="d-flex align-items-center mt-3 mt-sm-0">
+              <label for="itemsPerPage" class="me-2 mb-0">{{ $t('items-per-page') }}</label>
+              <select id="itemsPerPage" v-model="itemsPerPage" class="form-select d-inline-block w-auto">
+                <option v-for="size in [5, 10, 15, 20]" :key="size" :value="size">
+                  {{ size }}
+                </option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
@@ -91,6 +99,29 @@ export default {
   computed: {
     totalPages() {
       return Math.ceil(this.activities.length / this.itemsPerPage);
+    },
+    visiblePages() {
+      const maxVisiblePages = 5;
+      const pages = [];
+
+      if (this.totalPages <= maxVisiblePages) {
+        for (let i = 1; i <= this.totalPages; i++) {
+          pages.push(i);
+        }
+      } else {
+        const start = Math.max(2, this.currentPage - 1);
+        const end = Math.min(this.totalPages - 1, this.currentPage + 1);
+
+        pages.push(1);
+        if (start > 2) pages.push('...');
+        for (let i = start; i <= end; i++) {
+          pages.push(i);
+        }
+        if (end < this.totalPages - 1) pages.push('...');
+        pages.push(this.totalPages);
+      }
+
+      return pages;
     },
     paginatedActivities() {
       const start = (this.currentPage - 1) * this.itemsPerPage;
